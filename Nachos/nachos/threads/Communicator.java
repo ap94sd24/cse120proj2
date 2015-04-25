4851cba416ab86ff3ed2fa8183935f0b090413e9
AdamPan <adampan94@gmail.com>
2015-04-24 16:16:43 -0800

    "Communicator Test Cases Added"

--- Nachos/nachos/threads/Communicator.java
+++ Nachos/nachos/threads/Communicator.java
@@ -38,4 +38,46 @@ public class Communicator {
     public int listen() {
 	return 0;
     }
+    
+    public static void selfTest(){
+       final Communicator com = new Communicator();
+       final long times[] = new long[4];
+       final int words[] = new int[2];
+        KThread speaker1 = new KThread( new Runnable () {
+            public void run() {
+                com.speak(4);
+                times[0] = Machine.timer().getTime();
+            }
+        });
+        speaker1.setName("S1");
+        KThread speaker2 = new KThread( new Runnable () {
+            public void run() {
+                com.speak(7);
+                times[1] = Machine.timer().getTime();
+            }
+        });
+        speaker1.setName("S2");
+        KThread listener1 = new KThread( new Runnable () {
+            public void run() {
+                words[0] = com.listen();
+                times[2] = Machine.timer().getTime();
+            }
+        });
+        listener1.setName("L1");
+        KThread listener2 = new KThread( new Runnable () {
+            public void run() {
+                words[1] = com.listen();
+                times[3] = Machine.timer().getTime();
+            }
+        });
+        listener2.setName("L2");
+        
+        speaker1.fork(); speaker2.fork(); listener1.fork(); listener2.fork();
+        speaker1.join(); speaker2.join(); listener1.join(); listener2.join();
+        
+        Lib.assertTrue(words[0] == 4, "Didn't listen back spoken word."); 
+        Lib.assertTrue(words[1] == 7, "Didn't listen back spoken word.");
+        Lib.assertTrue(times[0] < times[2], "speak returned before listen.");
+        Lib.assertTrue(times[1] < times[3], "speak returned before listen.");
+    }
 }
